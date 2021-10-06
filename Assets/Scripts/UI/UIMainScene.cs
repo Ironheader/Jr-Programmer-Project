da7ecf2a4f3fc6cf0fe05e64c1f7a3978879610f
Timarco Ross <timarcosponge2@gmail.com>
2021-10-06 14:53:59 -0400

    Move Back and Forth Between Scenes
    -Implemented Scene Transition Buttons from Menu to Game
    -Implemented Scene Transition from Game to Menu
    -Implemented A button that make you quit game.

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,8 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
